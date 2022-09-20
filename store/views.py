1bf9471a89a3fca6e452eaec3533f6132a105baf
Daniel_Fujarski <funtixa@gmail.com>
2022-09-20 23:06:41 +0200

    search added

--- store/views.py
+++ store/views.py
@@ -1,8 +1,18 @@
+from django.db.models import Q
 from unicodedata import category
 from django.shortcuts import render, get_object_or_404
 
 from .models import Category, Product
 
+
+def search(request):
+    query = request.GET.get('query', '')
+    products = Product.objects.filter(Q( title__icontains=query) | Q(description__icontains=query))
+    context = {
+        'query': query,
+        'products': products,
+    }
+    return render(request, 'store/search.html',context)
 
 def category_detail(request, slug):
     category = get_object_or_404(Category, slug=slug)
